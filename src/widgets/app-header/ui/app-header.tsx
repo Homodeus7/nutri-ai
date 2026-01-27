d51d6265dfd7ac480a256d0dd938f90d9e5ae672
plumbus <plum.bus@yahoo.com>
2026-01-27 15:22:06 +0700

    fix: adaptive styles

--- src/widgets/app-header/ui/app-header.tsx
+++ src/widgets/app-header/ui/app-header.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { UiButton } from "@/shared/ui/ui-button";
-import { Bell } from "lucide-react";
 import Link from "next/link";
 import { NutriAiLogo } from "@/shared/ui";
 import { ROUTER_PATHS } from "@/shared/constants/routes";
@@ -10,8 +8,8 @@ import { AccountMenu } from "@/widgets/account-menu";
 export function AppHeader() {
   return (
     <header className="bg-card border-b border-border sticky top-0 z-50">
-      <div className="px-8 py-4 flex items-center justify-between md:justify-end">
-        <div className="md:hidden">
+      <div className="px-8 py-4 flex items-center justify-between xl:justify-end">
+        <div className="xl:hidden">
           <Link href={ROUTER_PATHS.DIARY}>
             <NutriAiLogo width={100} height={24} />
           </Link>
