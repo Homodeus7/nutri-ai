a8c99a7f0fb7fbffe6a0c17f28a163a8f634758f
plumbus <plum.bus@yahoo.com>
2026-01-29 10:59:47 +0700

    fix: use-calorie-data

--- src/pages/diary/model/use-calorie-data.ts
+++ src/pages/diary/model/use-calorie-data.ts
@@ -1,6 +1,7 @@
 import { useDayData } from "@/features/day-data";
 import { useAuthStore } from "@/entities/auth";
 import { useSelectedDate } from "@/entities/select-date";
+import { useGoals } from "@/features/goals";
 
 const DEFAULT_CALORIE_GOAL = 2500;
 
@@ -14,9 +15,12 @@ export function useCalorieData() {
     date: selectedDateString,
   });
 
+  const { data: userGoals } = useGoals();
+
   const consumedKcal = dayData?.consumedKcal ?? 0;
 
-  const userGoal = user?.dailyKcalGoal;
+  const userGoal = userGoals?.dailyKcalGoal;
+
   const targetKcal =
     dayData?.targetKcal ||
     (userGoal && userGoal > 0 ? userGoal : DEFAULT_CALORIE_GOAL);
